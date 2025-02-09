61b278b91885c8c615ad0548bb14bf2d7b3c668d
FlyingNutcase <flying.nutcase@gmail.com>
2025-02-09 16:19:39 +1300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace FogOfWarConsole
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Welcome to Fog of War Console App!");
+            Console.WriteLine("Press and key to exit...");
+            Console.ReadKey();
+        }
+    }
+
+}
+
